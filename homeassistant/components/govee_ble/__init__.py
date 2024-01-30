d8590dc1f3ff4c94dbe19b51d6bcce7ec01c1000
Scott Kilau <scott.kilau@gmail.com>
2024-01-30 12:34:31 -0600

    Add support for the Govee Buttons/Events

--- homeassistant/components/govee_ble/__init__.py
+++ homeassistant/components/govee_ble/__init__.py
@@ -3,21 +3,74 @@ from __future__ import annotations
 
 import logging
 
-from govee_ble import GoveeBluetoothDeviceData
+from govee_ble import GoveeBluetoothDeviceData, SensorUpdate
 
-from homeassistant.components.bluetooth import BluetoothScanningMode
+from homeassistant import config_entries
+from homeassistant.components.bluetooth import (
+    DOMAIN as BLUETOOTH_DOMAIN,
+    BluetoothScanningMode,
+    BluetoothServiceInfoBleak,
+)
 from homeassistant.components.bluetooth.passive_update_processor import (
     PassiveBluetoothProcessorCoordinator,
 )
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.device_registry import (
+    CONNECTION_BLUETOOTH,
+    DeviceRegistry,
+    async_get,
+)
 
-from .const import DOMAIN
+from .const import DOMAIN, GOVEE_BLE_EVENT, GoveeBleEvent
 
 PLATFORMS: list[Platform] = [Platform.SENSOR]
 
 _LOGGER = logging.getLogger(__name__)
+
+
+def process_service_info(
+    hass: HomeAssistant,
+    entry: config_entries.ConfigEntry,
+    data: GoveeBluetoothDeviceData,
+    service_info: BluetoothServiceInfoBleak,
+    device_registry: DeviceRegistry,
+) -> SensorUpdate:
+    """Process a BluetoothServiceInfoBleak, running side effects and returning sensor data."""
+    update = data.update(service_info)
+
+    if update.events:
+        address = service_info.device.address
+        for device_key, event in update.events.items():
+            sensor_device_info = update.devices[device_key.device_id]
+            device = device_registry.async_get_or_create(
+                config_entry_id=entry.entry_id,
+                connections={(CONNECTION_BLUETOOTH, address)},
+                identifiers={(BLUETOOTH_DOMAIN, address)},
+                manufacturer=sensor_device_info.manufacturer,
+                model=sensor_device_info.model,
+                name=sensor_device_info.name,
+                sw_version=sensor_device_info.sw_version,
+                hw_version=sensor_device_info.hw_version,
+            )
+            event_class = event.device_key.key
+            event_type = event.event_type
+
+            hass.bus.async_fire(
+                GOVEE_BLE_EVENT,
+                dict(
+                    GoveeBleEvent(
+                        device_id=device.id,
+                        address=address,
+                        event_class=event_class,  # ie 'button'
+                        event_type=event_type,  # ie 'press'
+                        event_properties=event.event_properties,
+                    )
+                ),
+            )
+
+    return update
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
@@ -25,6 +78,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     address = entry.unique_id
     assert address is not None
     data = GoveeBluetoothDeviceData()
+    device_registry = async_get(hass)
     coordinator = hass.data.setdefault(DOMAIN, {})[
         entry.entry_id
     ] = PassiveBluetoothProcessorCoordinator(
@@ -32,7 +86,9 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         _LOGGER,
         address=address,
         mode=BluetoothScanningMode.ACTIVE,
-        update_method=data.update,
+        update_method=lambda service_info: process_service_info(
+            hass, entry, data, service_info, device_registry
+        ),
     )
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     entry.async_on_unload(
--- homeassistant/components/govee_ble/const.py
+++ homeassistant/components/govee_ble/const.py
@@ -1,3 +1,20 @@
 """Constants for the Govee Bluetooth integration."""
+from __future__ import annotations
+
+from typing import Final, TypedDict
 
 DOMAIN = "govee_ble"
+
+EVENT_PROPERTIES: Final = "event_properties"
+EVENT_TYPE: Final = "event_type"
+GOVEE_BLE_EVENT: Final = "govee_ble_event"
+
+
+class GoveeBleEvent(TypedDict):
+    """Govee BLE event data."""
+
+    device_id: str
+    address: str
+    event_class: str  # ie 'button'
+    event_type: str  # ie 'press'
+    event_properties: dict[str, str | int | float | None] | None
